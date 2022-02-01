0d4f87b95579b32adc76b8186aca1bdc09e4bd48
zarin <81378664+zarintaufique@users.noreply.github.com>
2022-02-01 12:19:58 +0500

    Header file created and completed

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void type_prompt();
+
+void read_command(char cmd[], char* par[]);
+
+int parsePath(char* dirs[]);
+
+char* lookupPath(char* argv[], char* dir[]);
+
+#endif
